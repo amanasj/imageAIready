fbfa234a5d7e6c541bff781afe06bf6cf8e760ab
amanasj <67763567+amanasj@users.noreply.github.com>
2023-04-12 12:49:13 +0100

    Update mosaic.R

--- R/mosaic.R
+++ R/mosaic.R
@@ -41,7 +41,7 @@ mosaic <- function(patched_images_folder, dir=dirname(patched_images_folder)){
   
   
   for (i in 1:length(filenames0)){
-    #i=2
+    i=3
     patch_list_by_filename <- list.files(patched_images_folder, 
                                          pattern = paste0(filenames0[i]),
                                          full.names = T)
@@ -49,8 +49,14 @@ mosaic <- function(patched_images_folder, dir=dirname(patched_images_folder)){
     
     list2 <- list()
     for(j in 1:(length(patch_list_by_filename))){ 
-      #j=8
-      rx <- raster(patch_list_by_filename[j])
+      #j=4
+      rx <- suppressWarnings(raster::brick(patch_list_by_filename[j]))
+      if(rx@file@nbands==4){
+        rx <- suppressWarnings(raster::brick(rx[[1]],rx[[2]],rx[[3]]))
+      }else{
+        rx <- suppressWarnings(raster::brick(rx[[1]]))
+      }
+      
       list2[[j]] <- rx
       ## extract the patch position
       position <- stringr::str_extract_all(basename(patch_list_by_filename[j]), 
@@ -58,20 +64,21 @@ mosaic <- function(patched_images_folder, dir=dirname(patched_images_folder)){
       row_no <- as.numeric(position[1])
       col_no <- as.numeric(position[2])
       extent(list2[[j]]) <- extent(c((col_no-1)*256, (col_no)*256, (-row_no-1)*256, (-row_no)*256))
-    }
+      #plot(rx)
+      }
 
     # mosaic them, plot mosaic & save output
     list2$fun  <- max
-    rast.mosaic <- do.call(terra::mosaic, list2)
-    #plot(rast.mosaic, axes=F, legend=F, bty="n", box=FALSE)
-    writeRaster(rast.mosaic, filename=paste0(img_output_directory, filenames0[i]), 
-                format="GTiff", datatype='INT1U', overwrite=TRUE)
+    rast.mosaic <-suppressWarnings(do.call(terra::mosaic, list2))
     
-  
+    #plot(rast.mosaic)
+    terra::plotRGB(rast.mosaic)
+    writeRaster(rast.mosaic, filename=paste0(img_output_directory, filenames0[i], ".tiff"), 
+                overwrite=TRUE, datatype='INT1U')
+    
+
   }
   
   
   
 }
-
-
