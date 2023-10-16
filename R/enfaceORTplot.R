ee009ecbacdcf228928c2e756e9ccb71c6462b12
Aman Josan <67763567+amanasj@users.noreply.github.com>
2023-10-16 17:23:10 +0100

    Add files via upload

---
+++ R/enfaceORTplot.R
@@ -0,0 +1,170 @@
+
+##########################################################################################
+######################## Find ORT and overlay onto original images ##########################
+##########################################################################################
+
+enfaceORTplot <- function(ORT_data_F,
+                          heyex_images_folder,
+                          destin=dirname(heyex_images_folder))
+  {
+  
+
+  # install and load raster
+  if (!require("raster")){
+    install.packages("raster")
+    library(raster)
+    suppressPackageStartupMessages({library(raster)})
+  }
+  # install and load terra
+  if (!require("terra")){
+    install.packages("terra")
+    library(terra)
+    suppressPackageStartupMessages({library(terra)})
+  }
+  # install and load tidyverse
+  if (!require("tidyverse")){
+    install.packages("tidyverse")
+    library(tidyverse)
+    suppressPackageStartupMessages({library(tidyverse)})
+  }
+  # install and load magick
+  if (!require("magick")){
+    install.packages("magick")
+    library(magick)
+    suppressPackageStartupMessages({library(magick)})
+  }
+  # install and load imager
+  if (!require("imager")){
+    install.packages("imager")
+    library(imager)
+    suppressPackageStartupMessages({library(imager)})
+  }
+  # install and load gtools
+  if (!require("gtools")){
+    install.packages("gtools")
+    library(gtools)
+    suppressPackageStartupMessages({library(gtools)})
+  }
+  # install and load ggforce
+  if (!require("ggforce")){
+    install.packages("ggforce")
+    library(ggforce)
+    suppressPackageStartupMessages({library(ggforce)})
+  }
+  # install and load readheyexxml
+  if (!require("readheyexxml")){
+    install.packages("devtools")
+    library(devtools)
+    install_github("amanasj/readheyexxml")
+    library(readheyexxml)
+  }
+  # install and load grid
+  if (!require("grid")){
+    install.packages("grid")
+    library(grid)
+    suppressPackageStartupMessages({library(grid)})
+  }
+  
+  
+
+
+
+#### use readheyexxml function
+#source("C:\\Users\\ajosan\\OneDrive - Nexus365\\Desktop\\R_scripts\\readheyexxml\\Rscripts\\readheyexxml.R")
+heyex_data <- readheyexxml::readheyexxml(folder = heyex_images_folder)
+
+### find bscan number (y-position) by matching to first dataframe created by readheyex function
+df_ORT <- merge(heyex_data$data, ORT_data_F, by=c("ExamURL"))
+df_ORT <- df_ORT[gtools::mixedorder(df_ORT$ID),]
+df_ORT[c(2:5,7:18)] <- sapply(df_ORT[c(2:5,7:18)],as.numeric)
+df_ORT$x_coord <- (df_ORT$ORT_x * df_ORT$scalex_enface[1]) + df_ORT$plot_start_x[1]
+
+ORT_count <- nrow(df_ORT)
+
+ ######
+ ######
+ # Note that the first bscan starts at the bottom of the enface image even though the (0,0) coord 
+ # is the top left corner. 
+ ######
+ ######
+
+### Display enface image
+enface_image <- EBImage::readImage(df_ORT$ExamURL_enface[1])
+dim <- dim(enface_image)
+h <- dim[1]
+w <- dim[2]
+plot(enface_image)
+h1 <- df_ORT$h1[1]
+h2 <- df_ORT$h2[1]
+v1 <- df_ORT$v1[1]
+v2 <- df_ORT$v2[1]
+abline(h = h1, v = v1, col= "ivory")
+abline(h = h2, v = v2, col= "ivory")
+
+###############################################################
+################## prepare enface image #######################
+###############################################################
+### prepare enface image - add an alpha term to enface image
+enface_image = abind::abind(enface_image, enface_image[,,1])
+### set transparency with alpha 
+enface_image[,,4] = 0.75
+#EBImage::display(enface_img_crop)
+### weird matrix rotation occurs in rasterGrob below so pre-rotate
+enface_img_rotated <- EBImage::transpose(enface_image)
+#enface_img_rotated <- enface_image
+### use rasterGrob to prepare image for overlaying with ggplot
+### using annotation_custom
+### calc image centring positions x & y in rasterGrob
+g <- grid::rasterGrob(enface_img_rotated, 
+                      x = unit(0.5, "npc"), 
+                      y = unit(0.5, "npc"),
+                      height = 1, width = 1)
+
+
+
+coord_cartesian <- coord_cartesian(
+  xlim = c(0, w*heyex_data$data$scalex_enface[1]),
+  ylim = c(h*heyex_data$data$scaley_enface[1], 0),
+  expand = F, clip="on")
+#############################################################
+######################## ggplot #############################
+ORT_plot <- 
+  ggplot(data=df_ORT) +
+  geom_circle(aes(x0 = x_coord, 
+                  y0 = y_coord, 
+                  r=0.06), fill="green4") +
+  coord_fixed() +
+  #geom_polygon(data=df_ort)+
+  labs(x = "x (mm)", y = "y (mm)") +
+  ggtitle(paste0("ORT - ", eye)) +
+  coord_cartesian +
+  theme(aspect.ratio=1) +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        plot.title = element_text(size=22)) +
+
+
+  
+  geom_hline(yintercept = df_ORT$plot_start_y[1], linewidth=1, col="black") +
+  geom_hline(yintercept = df_ORT$plot_end_y[1], linewidth=1, col="black") +
+  geom_vline(xintercept = df_ORT$plot_start_x[1], linewidth=1, col="black")+
+  geom_vline(xintercept = df_ORT$plot_end_x[1], linewidth=1, col="black")
+
+
+### add semi-transparent enface image to ggplot
+ORT_plot <- ORT_plot+annotation_custom(g)
+
+plot(ORT_plot)
+
+#predictions_folder <- "C:\\Users\\ajosan\\OneDrive - Nexus365\\Desktop\\R_scripts\\imageAIready"
+ggsave(paste0("ORT_enface_", eye, "_", timepoint, ".png"), dpi=300, path = paste0(destin,"\\",eye), 
+       width = 2500, height = 2500, units = "px")
+
+
+
+
+} ## closes enfaceORTplot func 
+
+
+
---
+++ R/findORT.R
@@ -0,0 +1,161 @@
+
+#############################################################################
+######################## Find ORT  ##########################
+#############################################################################
+
+
+findORT <- function(predictions,
+                    heyex_images_folder, 
+                    bbox_full_images_folder,
+                    ORT_size_min = 100){
+  
+  
+  # install and load magick
+  if (!require("magick")){
+    install.packages("magick")
+    library(magick)
+    suppressPackageStartupMessages({library(magick)})
+  }
+  # install and load tidyverse
+  if (!require("tidyverse")){
+    install.packages("tidyverse")
+    library(tidyverse)
+    suppressPackageStartupMessages({library(tidyverse)})
+  }
+  # install and load imager
+  if (!require("imager")){
+    install.packages("imager")
+    library(imager)
+    suppressPackageStartupMessages({library(imager)})
+  }
+  # install and load imager
+  if (!require("sp")){
+    install.packages("sp")
+    library(sp)
+    suppressPackageStartupMessages({library(sp)})
+  }
+  
+  
+    
+  
+  
+  ##### create lists for later use
+  ORT <- list()
+  ORT_df <- data.frame()
+  ORT_data_F <- data.frame()
+  ORT_coords <- list()
+
+    
+
+  
+#### cycle through all images
+
+  for (i in 1:length(predictions$summary$filename)){        ### cycles through all patches
+    #i=12
+
+      
+      ################################################################
+      #################### find ORT positions ########################
+        
+        ################ find location of binary ORT
+        img <- magick::image_read(predictions$summary$filename[i])
+        binary <- magick::image_read(predictions$prediction_binary[[i]])
+        filei <- basename(predictions$summary$filename[i])
+        ExamURL <- stringr::str_extract(filei, "[^_]+")
+        ExamURL <- paste0(ExamURL, ".tif")
+        ExamURL_full <- paste0(heyex_images_folder, "\\", ExamURL)
+        ### find how wide original image was
+        meta_orig <- magick::image_read(ExamURL_full)
+        w_orig <- as.numeric(magick::image_info(meta_orig)[2])
+        h_orig <- as.numeric(magick::image_info(meta_orig)[3])
+        ExamURL_bbox <- paste0(bbox_full_images_folder, ExamURL)
+        meta_bbox <- magick::image_read(ExamURL_bbox)
+        w_bbox <- as.numeric(magick::image_info(meta_bbox)[2])
+        h_bbox <- as.numeric(magick::image_info(meta_bbox)[3])
+        px <- as.raster(binary)
+        px <- suppressWarnings(imager::as.cimg(px)) %>% plot()
+        px <- threshold(px, thr = "95%") %>% plot
+        px <- px>0.1
+        w <- as.numeric(magick::image_info(img)[2])
+        h <- as.numeric(magick::image_info(img)[3])
+        if (sum(px)==0) {
+          ORT[i] <- list(list(list(c(0,0,0))))
+        } else { 
+          sp <- imager::split_connected(px)
+            if(length(sp)!=0){
+          ### loop over all split connected regions (masks) to apply bounding box to each
+          ORT_coords_bbox <- list()
+          ORT_coords_bbox_temp <- list()  
+          for (k in 1:length(sp)){  
+            #k=1
+            #highlight(sp[[k]])
+            bbox(sp[[k]]) %>% highlight(col="yellow")
+            box <- where(bbox(sp[[k]])) 
+            box <- box[c(box$cc==1),]
+            box_width <- max(box$x) - min(box$x)
+            box_height <- max(box$y) - min(box$y)
+            box_size <- box_width * box_height
+            if(is.null(ORT_size_min)==TRUE || box_size > ORT_size_min){
+            box_x_centre <- min(box$x) + ((max(box$x) - min(box$x)) / 2)
+            box_y_centre <- min(box$y) + ((max(box$y) - min(box$y)) / 2)
+            }else{box_x_centre <- NA
+                  box_y_centre <- NA}
+          
+          ### isolate patch position
+          # Get the parenthesis and what is inside
+          brackets <- stringr::str_extract(filei, "(?<=\\().*(?=\\))")
+          brackets <- scan(text = brackets, sep = ",", what = "")
+          brackets <- as.numeric(brackets)
+          patch_vert <- brackets[1]
+          patch_horiz <- brackets[2]
+          ### infer no. of tiles horiz and vert (remember images were possibly resized
+          ### so figures won't necessarily be integer values - so round off)
+          tiles_horiz <- w_bbox / w 
+          tiles_vert <-  h_bbox / h
+          ### so ORT position in overall image given by
+          ORT_horiz_bbox <- ((w_bbox/tiles_horiz)*(patch_horiz-1))+box_x_centre
+          ORT_vert_bbox <- ((h_bbox/tiles_vert)*(patch_vert-1))+box_y_centre
+          ORT_horiz_orig <- (ORT_horiz_bbox/w_bbox) * w_orig
+          ORT_vert_orig <-  (ORT_vert_bbox/h_bbox) * h_orig
+          ORT_coords_one <- cbind(file=ExamURL, x=ORT_horiz_orig, y=ORT_vert_orig)
+          ORT_coords_temp <- list(ORT_coords_one)
+          ORT_coords_temp <- ORT_coords_temp[lapply(ORT_coords_temp,length)>0]
+          
+
+          ORT_coords[k] <- list(ORT_coords_temp) 
+          
+                    }    ## loop over k ORTs per patch   
+      
+               ORT[i] <- list(ORT_coords) 
+            }else{ORT[i] <- list(list(list(c(0,0,0))))}
+          } 
+          
+
+  
+
+
+  ORT_df <- data.frame(matrix(unlist(ORT), nrow=sum(lengths(ORT)), byrow=TRUE))
+  colnames(ORT_df) <- c("ExamURL", "ORT_x","ORT_y")
+  ORT_df[2:3] <- sapply(ORT_df[2:3],as.numeric)
+
+      
+      
+      } ## loop over i patches
+  
+  
+
+  ORT_data_F <- ORT_df
+  ### remove blank or NA or zero rows
+  ORT_data_F[ORT_data_F == ""] <- NA
+  ORT_data_F <- na.omit(ORT_data_F)
+  ORT_data_F <- ORT_data_F[ORT_data_F$ExamURL != 0, ]
+
+  return(ORT_data_F)
+  
+  
+  
+  
+} ## end findORT function 
+
+
+
